12e9334e56d9cb43ecf89a61d039cd977e0e3e80
Simon Ellmann <simon.ellmann@tum.de>
2020-01-26 01:28:52 +0100

    Update settings

--- registration/settings.py
+++ registration/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'participant',
+    'participant.apps.ParticipantConfig',
 ]
 
 MIDDLEWARE = [
