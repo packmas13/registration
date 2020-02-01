0cc8fcf19d709486147394ce7a2bbb3c632cca5d
Simon Ellmann <simon.ellmann@tum.de>
2020-02-01 15:35:47 +0100

    Include features from #9 into import command

--- registration/management/commands/import.py
+++ registration/management/commands/import.py
@@ -13,29 +13,47 @@ class Command(BaseCommand):
         parser.add_argument('filename')
 
     def handle(self, *args, **options):
+        created_users, created_troops = 0, 0
+
         try:
-            with open(options['filename'], newline='') as csvfile:
-                csvreader = csv.reader(csvfile, delimiter=';')
+            with open(options['filename'], newline='') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=';')
+                columns = self._extract_columns(next(csv_reader))
 
                 with transaction.atomic():
-                    for i, row in enumerate(csvreader):
+                    for i, row in enumerate(csv_reader):
                         try:
-                            t = Troop()
+                            if not Troop.objects.filter(number=row[columns['troop_number']]).exists():
+                                t = Troop()
 
-                            t.number = row[0]
-                            t.name = row[1]
+                                t.number = row[columns['troop_number']]
+                                t.name = row[columns['troop_name']]
 
-                            t.save()
+                                t.save()
+                                created_troops += 1
 
-                            user = get_user_model().objects.create_user(email=row[4])
+                            user = get_user_model().objects.create_user(email=row[columns['email']])
 
                             user.is_active = False
-                            user.first_name = row[2]
-                            user.last_name = row[3]
+                            user.first_name = row[columns['firstname']]
+                            user.last_name = row[columns['lastname']]
 
                             user.save()
+                            created_users += 1
                         except Exception as e:
-                            self.stderr.write(self.style.ERROR('Line {}: {}'.format(i+1, e.__cause__)))
-                            return
+                            raise CommandError('Line {}: {}'.format(i+2, e.__cause__))
         except FileNotFoundError:
-            self.stderr.write(self.style.ERROR('File not found'))
+            raise CommandError('File not found')
+
+        self.stdout.write(f'Imported {created_troops} new troops and {created_users} new users successfully')
+
+    def _extract_columns(self, row) -> dict:
+        required = ['email', 'firstname', 'lastname', 'troop_number', 'troop_name']
+        missing = [name for name in required if name not in row]
+
+        if missing:
+            msg = 'Found the following columns in the csv: {}\n' \
+                  'Required columns missing: {}'.format(', '.join(row), ', '.join(missing))
+            raise CommandError(msg)
+
+        return {name: i for i, name in enumerate(row)}
