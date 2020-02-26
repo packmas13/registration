0eaecc7a00a528bd70a265f1543c963f28252fd9
oliverpool <oliverpool@hotmail.fr>
2020-02-26 21:16:55 +0100

    1300 * 100

--- troop/tests.py
+++ troop/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 
 class IndexTest(TestCase):
-    fixtures = ["troop_1300.json"]
+    fixtures = ["troop_130000.json"]
 
     def setUp(self):
         self.user = get_user_model().objects.get(email="user@test")
@@ -20,16 +20,16 @@ class IndexTest(TestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_not_allowed(self):
-        response = self.client.get(reverse("troop:index", kwargs={"troop": 1301}))
+        response = self.client.get(reverse("troop:index", kwargs={"troop": 130100}))
         self.assertEqual(response.status_code, 403)
 
     def test_found(self):
-        response = self.client.get(reverse("troop:index", kwargs={"troop": 1300}))
+        response = self.client.get(reverse("troop:index", kwargs={"troop": 130000}))
         self.assertEqual(response.status_code, 200)
 
 
 class CreateParticipantTest(TestCase):
-    fixtures = ["troop_1300.json"]
+    fixtures = ["troop_130000.json"]
 
     valid_data = {
         "troop": "1",  # id of the troop
@@ -53,39 +53,39 @@ class CreateParticipantTest(TestCase):
 
     def test_get_form(self):
         response = self.client.get(
-            reverse("troop:participant.create", kwargs={"troop": 1300})
+            reverse("troop:participant.create", kwargs={"troop": 130000})
         )
         self.assertEqual(response.status_code, 200)
 
     def test_post_empty_form(self):
         response = self.client.post(
-            reverse("troop:participant.create", kwargs={"troop": 1300})
+            reverse("troop:participant.create", kwargs={"troop": 130000})
         )
         self.assertEqual(response.status_code, 422)
 
     def test_post_form(self):
         response = self.client.post(
-            reverse("troop:participant.create", kwargs={"troop": 1300}), self.valid_data
+            reverse("troop:participant.create", kwargs={"troop": 130000}), self.valid_data
         )
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
-            response.url, reverse("troop:participant.index", kwargs={"troop": 1300})
+            response.url, reverse("troop:participant.index", kwargs={"troop": 130000})
         )
 
     def test_post_form_addanother(self):
         data = self.valid_data.copy()
         data["_addanother"] = "1"
         response = self.client.post(
-            reverse("troop:participant.create", kwargs={"troop": 1300}), data
+            reverse("troop:participant.create", kwargs={"troop": 130000}), data
         )
         self.assertEqual(response.status_code, 302)
         self.assertEqual(
-            response.url, reverse("troop:participant.create", kwargs={"troop": 1300})
+            response.url, reverse("troop:participant.create", kwargs={"troop": 130000})
         )
 
 
 class IndexParticipantTest(TestCase):
-    fixtures = ["troop_1300.json"]
+    fixtures = ["troop_130000.json"]
 
     def setUp(self):
         self.user = get_user_model().objects.get(email="user@test")
@@ -93,24 +93,24 @@ class IndexParticipantTest(TestCase):
 
     def test_found(self):
         response = self.client.get(
-            reverse("troop:participant.index", kwargs={"troop": 1300})
+            reverse("troop:participant.index", kwargs={"troop": 130000})
         )
         self.assertEqual(response.status_code, 200)
 
     def test_contains_creations(self):
         response = self.client.get(
-            reverse("troop:participant.index", kwargs={"troop": 1300})
+            reverse("troop:participant.index", kwargs={"troop": 130000})
         )
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, "Trick")
 
         self.client.post(
-            reverse("troop:participant.create", kwargs={"troop": 1300}),
+            reverse("troop:participant.create", kwargs={"troop": 130000}),
             CreateParticipantTest.valid_data,
         )
 
         response = self.client.get(
-            reverse("troop:participant.index", kwargs={"troop": 1300})
+            reverse("troop:participant.index", kwargs={"troop": 130000})
         )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Trick")
