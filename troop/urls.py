85af19c7bf2b3d6313e40486c3b9bdf6bf1f3ea1
oliverpool <oliverpool@hotmail.fr>
2020-02-27 08:54:04 +0100

    singular participant

--- troop/urls.py
+++ troop/urls.py
@@ -5,17 +5,17 @@ app_name = "troop"
 urlpatterns = [
     path("<int:troop>/", views.IndexView.as_view(), name="index"),
     path(
-        "<int:troop>/participants/",
+        "<int:troop>/participant/",
         views.IndexParticipantView.as_view(),
         name="participant.index",
     ),
     path(
-        "<int:troop>/participants/<int:pk>/",
+        "<int:troop>/participant/<int:pk>/",
         views.UpdateParticipantView.as_view(),
         name="participant.edit",
     ),
     path(
-        "<int:troop>/participants/create/",
+        "<int:troop>/participant/create/",
         views.CreateParticipantView.as_view(),
         name="participant.create",
     ),
